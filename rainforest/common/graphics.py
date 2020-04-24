Fixes in the QPE computation

rainforest/common/graphics.py
@@ -80,7 +80,7 @@ class QPE_cmap( mpl.colors.LinearSegmentedColormap):
 
 def qpe_plot(data, subplots = None, figsize = None,
              vmin = 0.04, vmax = 120, transition = 10, ch_border = True,
-             **kwargs):
+             xlim = None, ylim = None, **kwargs):
     
     """Plots one or multiple QPE realizations using a special colormap, that
     shows a clear transition between low and high precipitation intensities,
@@ -166,10 +166,19 @@ def qpe_plot(data, subplots = None, figsize = None,
                 x = [i[0]/1000. for i in shape.shape.points[:]]
                 y = [i[1]/1000. for i in shape.shape.points[:]]
                 ax[i].plot(x,y,'k',linewidth=1.)
-                
-    plt.xlim([400,900])
-    plt.ylim([-0,350])
+    if xlim != None:
+        plt.xlim(xlim)
+    else:
+        plt.xlim([400,900])
+    if ylim != None:
+        plt.ylim(ylim)
+    else:
+        plt.ylim([0,350])
+        
+        
     fig.subplots_adjust(bottom = 0.2)
+    fig.subplots_adjust(wspace=0.1, hspace=0.1)
+    
     cbar_ax = fig.add_axes([0.18, 0.15, 0.7, 0.03])
     cbar=plt.colorbar(m,format='%.2f',orientation='horizontal', cax=cbar_ax,
                       norm = norm, extend = 'max')
@@ -249,7 +258,8 @@ def score_plot(scores, title_prefix = '', figsize = (10,5)):
         for j,s in enumerate(scorenames):
             for k, m in enumerate(models_reordered):
                 sc = scores[m][precip_range][s]
-                rec = ax[i].bar([offset*j+k],[sc], color = colors[k])
+                rec = ax[i].bar([offset*j+k],[sc], color = colors[k], 
+                                width = 1)
                 _autolabel(ax[i], rec)
                
                 
@@ -339,9 +349,13 @@ def qpe_scatterplot(ref, qpe_est, title_prefix = '', figsize = (10,7.5)):
         fig, ax = plt.subplots(1, len(models),
                                figsize = figsize,sharey=True, sharex = True)
         
-    plt.setp(ax.flat, aspect=1.0, adjustable='box')
-    ax = ax.ravel()
-
+    
+    try :
+        ax = ax.ravel()
+    except:
+        pass
+    plt.setp(ax, aspect=1.0, adjustable='box')
+    
     gmax = np.nanmax(ref)
     for i,m in enumerate(models_reordered):
         pl = ax[i].hexbin(qpe_est[m].ravel(), ref.ravel(), bins = 'log',
@@ -452,7 +466,7 @@ def plot_crossval_stats(stats, output_folder):
                  
                              rec = ax[i].bar([offset*j+k],[mean], 
                                              color = colors[k],
-                                             yerr = std)
+                                             yerr = std, width = 1.)
                              _autolabel(ax[i], rec)
                          x.append(offset*j+0.6)      
 

rainforest/common/utils.py
@@ -59,14 +59,11 @@ def perfscores(est_data, ref_data, bounds = None, array = False):
     """
     Computes a set of precipitation performance scores, on different data ranges.
     The scores are
-        - ME: mean error (linear bias)
-        - CORR: Pearson correlation of estimation and reference
-        - STDE: standard deviation of the errors
-        - MAE: mean absolute error
-        - bias:  (ME/mean(ref_data) + 1) in dB
         - scatter: 0.5 * (Qw84(x) - Qw16(x)), where Qw is a quantile weighted
           by ref_data / sum(ref_data) and x is est_data / ref_data in dB scale
-        - ED: the energy distance which is a measure of the distance between 
+        - RMSE: root mean  square error (linear error)
+        - bias:  (ME/mean(ref_data) + 1) in dB
+        - ED: the energy distance which is a measure of the distance between
           two distributions (https://en.wikipedia.org/wiki/Energy_distance)
           
     Parameters
@@ -123,25 +120,16 @@ def perfscores(est_data, ref_data, bounds = None, array = False):
 
 def _perfscores(est_data, ref_data):
     """An unbounded version of the previous function"""
-    linbias = np.nanmean(est_data - ref_data)
-    stde = np.nanstd(est_data - ref_data)
-    mae = np.nanmean(np.abs(est_data - ref_data))
-    est_data[est_data == 0] += 1E-6
-    db_err = 10 * np.log10(est_data / ref_data)
-    weights = ref_data / np.sum(ref_data)
-    scatter = 0.5 * (quantile(db_err,weights,0.84) - quantile(db_err,weights,0.16))
-    bias_db = 10 * np.log10(linbias/np.nanmean(ref_data) + 1)
-    corr = np.corrcoef(est_data, ref_data)[0,1]
-    ed = energy_distance(est_data, ref_data)
-    
-    metrics = {'ME' : linbias, 
-               'CORR' : corr, 
-               'STDE' : stde, 
-               'MAE' : mae, 
-               'scatter' : scatter,
-               'bias' : bias_db,  
-               'ED' : ed,
-               'N': len(est_data)}
+    doublecond = np.logical_and(ref_data > 0.1, est_data > 0.1)
+    rmse = np.sqrt(np.nanmean((est_data-ref_data)**2))
+    db_err = 10 * np.log10(est_data[doublecond] / ref_data[doublecond])
+    weights = ref_data[doublecond]/np.sum(ref_data[doublecond])
+    scatter = 0.5 * (quantile(db_err,weights,0.84) -quantile(db_err,weights,0.16))
+    bias_db = 10*np.log10(np.sum(est_data[doublecond]) / np.sum(ref_data[doublecond]))
+
+    ed = energy_distance(est_data[np.isfinite(est_data)], ref_data[np.isfinite(est_data)])
+    metrics = {'RMSE':rmse,'scatter':scatter,'logBias':bias_db,
+       'ED':ed,'N':len(ref_data)}
     
     return metrics
 

rainforest/qpe/evaluation.py
@@ -23,7 +23,7 @@ from ..common.utils import timestamp_from_datetime, nearest_time
 from ..common.lookup import get_lookup
 from ..common.io_data import read_cart
 from ..common.graphics import score_plot, qpe_scatterplot
-
+from ..common.retrieve_data import retrieve_CPCCV
 
 def evaluation(qpefolder, gaugepattern, list_models = None, 
                outputfolder = './', t0 = None, t1 = None,
@@ -58,7 +58,7 @@ def evaluation(qpefolder, gaugepattern, list_models = None,
                         
      
     """
-    logging.info('Getting all files from qpe folder')
+    logging.info('Getting all files from qpe folder {:s}'.format(qpefolder))
 
     tmp = get_qpe_files(qpefolder, time_agg = 10, list_models = list_models)
     # Get only timesteps where at least 2 files are available during 10 min period
@@ -77,7 +77,7 @@ def evaluation(qpefolder, gaugepattern, list_models = None,
         if nmodels[i] == max(nmodels):
             qpe_files10_filt[k] = qpe_files10[k]
             
-    models = list(qpe_files10_filt.values())[0].keys()
+    models = list(list(qpe_files10_filt.values())[0].keys())
     tsteps = sorted(list(qpe_files10_filt.keys()))
     
     logging.info('Reading gauge data from pattern {:s}'.format(gaugepattern))
@@ -128,11 +128,21 @@ def evaluation(qpefolder, gaugepattern, list_models = None,
     hours = np.array([nearest_time(t, 60) for t in tsteps])
     hours_u,cnt = np.unique(hours, return_counts = True)
     precip_qpe60 = {}
+
+        
     for m in models:
         data = precip_qpe[m]
         precip_qpe60[m] = np.array([np.nanmean(data[hours == h], axis = 0) 
             for h in hours_u[cnt == 6]] )
-
+    if 'CPC.CV' in list_models:
+        logging.info('Retrieving CPC.CV data. at hourly resolution...')
+        
+        precip_qpe60['CPC.CV'] = []
+        for h in hours_u:
+            precip_qpe60['CPC.CV'] .append(retrieve_CPCCV(h, stations))
+        precip_qpe60['CPC.CV']  = np.array(precip_qpe60['CPC.CV'])
+        models.append('CPC.CV')
+        
     precip_ref60 = np.array([np.nanmean(precip_ref[hours == h], axis = 0) 
             for h in hours_u[cnt == 6]] )
 

rainforest/qpe/qpe.py
@@ -196,15 +196,17 @@ class QPEProcessor(object):
         # Retrieve polar files and lookup tables for all radars
         for rad in self.config['RADARS']:
             logging.info('Retrieving data for radar '+rad)
-            
-            radfiles = retrieve_prod(self.config['TMP_FOLDER'], t0, t1, 
-                               product_name = 'ML' + rad, 
-                               sweeps = self.config['SWEEPS'])
-            self.radar_files[rad] = split_by_time(radfiles)
-            statfiles = retrieve_prod(self.config['TMP_FOLDER'], t0, t1, 
-                               product_name = 'ST' + rad, pattern = 'ST*.xml')
-            self.status_files[rad] = split_by_time(statfiles)
-    
+            try:
+                radfiles = retrieve_prod(self.config['TMP_FOLDER'], t0, t1, 
+                                   product_name = 'ML' + rad, 
+                                   sweeps = self.config['SWEEPS'])
+                self.radar_files[rad] = split_by_time(radfiles)
+                statfiles = retrieve_prod(self.config['TMP_FOLDER'], t0, t1, 
+                                   product_name = 'ST' + rad, pattern = 'ST*.xml')
+                self.status_files[rad] = split_by_time(statfiles)
+            except:
+                logging.error('Failed to retrieve data for radar {:s}'.format(rad))
+                
     def compute(self, output_folder, t0, t1, timestep = 5,
                                                     basename = 'RF%y%j%H%M'):
         """
@@ -232,6 +234,8 @@ class QPEProcessor(object):
         """
         
         for model in self.models.keys():
+            if self.config['ADVECTION_CORRECTION']:
+                model += '_AC'
             if not os.path.exists(str(Path(output_folder, model))):
                 os.makedirs(str(Path(output_folder, model)))
         
@@ -244,6 +248,7 @@ class QPEProcessor(object):
                             for i in range(n_incr + 1)])
         
         qpe_prev = {}
+        X_prev = {}
         
         for i, t in enumerate(timeserie): # Loop on timesteps
             logging.info('====')
@@ -343,6 +348,7 @@ class QPEProcessor(object):
                     except:
                         logging.error('Could not compute sweep {:d}'.format(sweep))
                         pass
+                        
            
             """Part four - RF prediction"""
             # Get QPE estimate
@@ -354,19 +360,39 @@ class QPEProcessor(object):
                     X.append(dat.ravel())
                 
                 X = np.array(X).T
-                X[np.isnan(X)] = 0
+                if i == 0:
+                    X_prev[k] = X
+                    
+                idx_zh = np.where(np.array(model.variables)
+                                  == 'zh_VISIB')[0][0]
+               
+                rproxy = (X[:,idx_zh]/constants.A_QPE)**(1/constants.B_QPE)
+                rproxy[np.isnan(rproxy)] = 0
+
+                Xcomb = np.nanmean((X_prev[k] , X),axis = 0)
+                X_prev[k]  = X
+                
+                Xcomb[np.isnan(Xcomb)] = 0
+                rproxy_mean = (Xcomb[:,idx_zh]/constants.A_QPE)**(1/constants.B_QPE)
+                rproxy_mean[np.isnan(rproxy_mean)] = 0
+                
+            
                 # Remove axis with only zeros
-                validrows = (X>0).any(axis=1)
+                validrows = (Xcomb>0).any(axis=1)
             
                 qpe = np.zeros((NBINS_X, NBINS_Y), dtype = np.float32).ravel()
                 try:
-                    qpe[validrows] = self.models[k].predict(X[validrows,:])
+                    qpe[validrows] = self.models[k].predict(Xcomb[validrows,:])
                 except:
                     logging.error('RF failed!')
                     pass
                 
+                # Rescale qpe through rproxy
+                qpe = qpe * rproxy / rproxy_mean
                 # Reshape to Cartesian grid
                 qpe = np.reshape(qpe, (NBINS_X, NBINS_Y))
+                
+                
                 
                 # Postprocessing
                 if self.config['OUTLIER_REMOVAL']:
@@ -376,12 +402,15 @@ class QPEProcessor(object):
                     qpe = gaussian_filter(qpe,
                        self.config['GAUSSIAN_SIGMA'])
             
-                             
-                if self.config['ADVECTION_CORRECTION'] and i > 0:
-                    qpe = _disaggregate(np.array([qpe_prev[k], qpe]))
-                
+                if i == 0:
+                    qpe_prev[k] = qpe
+                    
+                comp = np.array([qpe_prev[k].copy(), qpe.copy()])
                 qpe_prev[k] = qpe
-
+                if self.config['ADVECTION_CORRECTION'] and i > 0:
+                    qpe = _disaggregate(comp)
+                 
+                
                 tstr = datetime.datetime.strftime(t, basename)
                 filepath = output_folder + '/' + k
                 if self.config['ADVECTION_CORRECTION']: