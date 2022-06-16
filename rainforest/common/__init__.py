356586afa618fbcb627ffbca8ff2d57f0fa2200c
Rebecca Gugerli <rebecca.gugerli@meteoswiss.ch>
2022-06-16 10:55:37 +0200

    changed file because it caused an error

--- rainforest/common/__init__.py
+++ rainforest/common/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from retrieve_data import get_COSMO_T
-from retrieve_data import get_COSMO_variables
-from retrieve_data import retrieve_CPCCV
-from retrieve_data import retrieve_prod
+"""
+Created on Tue May  8 16:07:04 2018
+
+@author: wolfensb
+"""
