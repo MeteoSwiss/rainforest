0bc6728884195d705eea7364170cc0932868ce7d
Daniel Wolfensberger <daniel.wolfensberger@meteoswiss.ch>
2024-02-22 18:53:12 +0100

    solve circular import with __version__

--- rainforest/__init__.py
+++ rainforest/__init__.py
@@ -1,15 +1,7 @@
-import importlib.metadata as _importlib_metadata
-
 from . import qpe
 from . import common
 from . import database
 from . import ml
 from . import performance
 
-# Get the version
-try:
-    __version__ = _importlib_metadata.version("rainforest_mch")
-except _importlib_metadata.PackageNotFoundError:
-    # package is not installed
-    __version__ = "0.0.0"
-
+__version__ = common.utils.get_version()
