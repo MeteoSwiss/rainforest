738e01ca6e3721a0674345f3b7e746fe0cbc506b
cat <michele.cattaneo@meteoswiss.ch>
2024-11-07 17:21:21 +0100

    Fixed rf to new sklearn version, changed database to v2

--- rainforest/ml/rfdefinitions.py
+++ rainforest/ml/rfdefinitions.py
@@ -23,12 +23,12 @@ from ..common.object_storage import ObjectStorage
 ObjStorage = ObjectStorage()
 FOLDER_RF = Path(os.environ['RAINFOREST_DATAPATH'], 'rf_models')
 
-  
+
 ##################
-# Add here all classes/functions used in the construction of the pickled 
+# Add here all classes/functions used in the construction of the pickled
 # instances
 ##################
-    
+
 def _polyfit_no_inter(x,y, degree):
     """linear regression with zero intercept"""
     X = []
@@ -38,44 +38,44 @@ def _polyfit_no_inter(x,y, degree):
     p, _, _, _ = np.linalg.lstsq(X, y[:,None])
     p = np.insert(p,0,0) # Add zero intercept at beginning for compatibility with polyval
     return p[::-1] # Reverse because that's how it is in polyfit (high degree first)
-    
+
 class RandomForestRegressorBC(RandomForestRegressor):
     '''
     This is an extension of the RandomForestRegressor regressor class of
     sklearn that does additional bias correction, is able
-    to apply a rounding function to the outputs on the fly and adds a 
+    to apply a rounding function to the outputs on the fly and adds a
     bit of metadata:
-    
+
         *bctype* : type of bias correction method
         *variables* : name of input features
         *beta* : weighting factor in vertical aggregation
         *degree* : order of the polyfit used in some bias-correction methods
         *metadata* : configuration setup used to train this model
-    
+
     For *bc_type* tHe available methods are currently "raw":
     simple linear fit between prediction and observation, "cdf": linear fit
     between sorted predictions and sorted observations and "spline" :
     spline fit between sorted predictions and sorted observations. Any
     new method should be added in this class in order to be used.
-    
+
     For any information regarding the sklearn parent class see
-    
+
     https://github.com/scikit-learn/scikit-learn/blob/b194674c4/sklearn/ensemble/_forest.py#L1150
     '''
-    def __init__(self, 
-                 variables, 
+    def __init__(self,
+                 variables,
                  beta,
-                 visib_weighting, 
-                 degree = 1, 
+                 visib_weighting,
+                 degree = 1,
                  bctype = 'cdf',
                  metadata = {},
                  n_estimators=100,
-                 criterion="mse",
+                 criterion="squared_error",
                  max_depth=None,
                  min_samples_split=2,
                  min_samples_leaf=1,
                  min_weight_fraction_leaf=0.,
-                 max_features="auto",
+                 max_features="sqrt",
                  max_leaf_nodes=None,
                  min_impurity_decrease=0.,
                  bootstrap=True,
@@ -99,7 +99,7 @@ class RandomForestRegressorBC(RandomForestRegressor):
                          random_state = random_state,
                          verbose = verbose,
                          warm_start = warm_start)
-        
+
         self.degree = degree
         self.bctype = bctype
         self.variables = variables
@@ -126,7 +126,7 @@ class RandomForestRegressorBC(RandomForestRegressor):
         -------
         self : object
         """
-        
+        X.columns = [str(col) for col in X.columns]
         super().fit(X,y, sample_weight)
         y_pred = super().predict(X)
         if self.bctype in ['cdf','raw']:
@@ -144,9 +144,9 @@ class RandomForestRegressorBC(RandomForestRegressor):
             self.p = UnivariateSpline(x_[idx], y_[idx])
         else:
             self.p = 1
-            
-        return 
-    
+
+        return
+
     def predict(self, X, round_func = None, bc = True):
         """
         Predict regression target for X.
@@ -164,17 +164,17 @@ class RandomForestRegressorBC(RandomForestRegressor):
             (i.e. no function)
         bc : bool
             if True the bias correction function will be applied
-            
+
         Returns
         -------
         y : array-like of shape (n_samples,) or (n_samples, n_outputs)
             The predicted values.
         """
         pred = super().predict(X)
-        
+
         if round_func == None:
             round_func = lambda x: x
-        
+
         func = lambda x: x
         if bc:
             if self.bctype in ['cdf','raw']:
@@ -184,12 +184,12 @@ class RandomForestRegressorBC(RandomForestRegressor):
         out = func(pred)
         out[out < 0] = 0
         return round_func(out)
-    
+
 ##################
-        
+
 class MyCustomUnpickler(pickle.Unpickler):
     """
-    This is an extension of the pickle Unpickler that handles the 
+    This is an extension of the pickle Unpickler that handles the
     bookeeeping references to the RandomForestRegressorBC class
     """
     import __main__
@@ -197,41 +197,40 @@ class MyCustomUnpickler(pickle.Unpickler):
     def find_class(self, module, name):
         print(module,name)
         return super().find_class(module, name)
-    
-    
+
+
 def read_rf(rf_name, filepath=''):
     """
     Reads a randomForest model from the RF models folder using pickle. All custom
     classes and functions used in the construction of these pickled models
     must be defined in the script ml/rf_definitions.py
-    
+
     Parameters
     ----------
     rf_name : str
         Name of the randomForest model, it must be stored in the folder
         /ml/rf_models and computed with the rf:RFTraining.fit_model function
- 
+
     filepath: str
         Path to the model files, if not in default folder
-        
+
     Returns
     -------
-    A trained sklearn randomForest instance that has the predict() method, 
+    A trained sklearn randomForest instance that has the predict() method,
     that allows to predict precipitation intensities for new points
     """
-    
+
     if rf_name[-2:] != '.p':
         rf_name += '.p'
-    
-    if filepath == '': 
+
+    if filepath == '':
         if os.path.dirname(rf_name) == '':
             rf_name = str(Path(FOLDER_RF, rf_name))
     else:
         rf_name = str(Path(filepath, rf_name))
-        
+
     unpickler = MyCustomUnpickler(open(ObjStorage.check_file(rf_name), 'rb'))
     if not os.path.exists(rf_name):
         raise IOError('RF model {:s} does not exist!'.format(rf_name))
     else:
         return unpickler.load()
-      
